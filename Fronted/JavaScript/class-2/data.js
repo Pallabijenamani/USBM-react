0214bbe4b7b3a559910ce0cc015c4ded9e080ac7
pallabi jenamani <jenamanipallavi@gmail.com>
2024-10-07 14:13:47 +0530

    day 1

---
+++ Fronted/JavaScript/class-2/data.js
@@ -0,0 +1,54 @@
+
+//undefind
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let age=22;
+console.log("My age is : ",age);
+console.log(typeof age);
+let pi=3.14;
+console.log(pi);
+console.log(typeof pi);
+//let num=08; error because decimal with leading zero are not allowed here
+
+//Addition
+let num1=11;
+let num2=4;
+let addition=num1+num2;
+console.log(addition)
+//sub
+num1=11;
+ num2=4;
+let sub=num1-num2;
+console.log(sub);
+
+ num1=11;
+num2=4;
+ let mul=num1*num2;
+console.log(mul);
+
+ num1=11;
+num2=4;
+let div=num1/num2;
+console.log(div);
+
+// boolean
+     
+
+
+
+//string
+// let ch='Anjan';
+// let name="Ranjan";
+// console.log(ch,name)
+
+let name=" Anjan ";
+let ag =22;
+let stream="MCA";
+let roll=9;
+let city="Balasore";
+let clgName="USBM";
+console.log(`My name is ${name} ,my age is : ${age} , stream is ${stream},city name is ${city},college name is ${clgName} ,roll no is ${roll}`)
