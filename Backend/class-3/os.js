dd16f6a111e565245ec975d62ae15d64bb7214ae
pallabi jenamani <jenamanipallavi@gmail.com>
2024-12-11 12:19:30 +0530

    done

---
+++ Backend/class-3/os.js
@@ -0,0 +1,22 @@
+// os modules
+
+//import modules
+ const os = require("os");
+ console.log("os");
+ 
+ console.log(os.homedir()); //C:\Users\HP
+ console.log(os.hostname()); // ABHI
+ console.log(os.platform()); //win32
+ console.log(os.userInfo()); //[Object: null prototype] {
+//     uid: -1,
+//     gid: -1,
+//     username: 'HP',
+//     homedir: 'C:\\Users\\HP',
+//     shell: null
+//   }
+ console.log(os.totalmem()); //8210415616
+ console.log(os.freemem());  //1421406208
+ console.log(os.userInfo().username); //HP
+ 
+  
+ 
